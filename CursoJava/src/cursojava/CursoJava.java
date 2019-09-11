5ae156d443de1bedf83bdf92e6bc0b9ff7ea80af
LAB202 <LAB202@LAB206-PC13>
2019-09-11 09:34:30 -0500

    Mi primer proyecto del curso de java

---
+++ CursoJava/src/cursojava/CursoJava.java
@@ -0,0 +1,11 @@
+
+package cursojava;
+
+public class CursoJava {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Iniciando mi primer Repositorio :) ");
+    }
+    
+}
