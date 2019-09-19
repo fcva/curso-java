da8ae377827114f9bd309cb5d797a30a8e80dc8a
LAB202 <LAB202@LAB206-PC13>
2019-09-19 11:02:34 -0500

    Clase Alumno creado

---
+++ CursoJava/src/ejercicio03/Alumno.java
@@ -0,0 +1,43 @@
+
+package ejercicio03;
+
+/**
+ *
+ * @author LAB202
+ */
+public class Alumno {
+    
+    private int codigo;
+    
+    private String nombres;
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nombres
+     */
+    public String getNombres() {
+        return nombres;
+    }
+
+    /**
+     * @param nombres the nombres to set
+     */
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    
+    
+}
